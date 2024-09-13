c5cebe34ce96b6b12029690c5baae6c26f669747
deeptimay <deeptimayroutray@gmail.com>
2024-09-13 12:08:44 +0530

    Gradle & Library Setup | README file updated

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # ProductShowcase
+
+# Shortly
+A two page mobile app for feting product using the public APIs. With well handled loading, error states and retry capabilities.
+
+# Features:
+* Kotlin<br>
+* MVVM<br>
+* Retrieve Network Data from API (REST API)<br>
+* Use cases<br>
+* Unit Tests<br>
+* MockWebServer (Okhttp)<br>
+* Kotlin Flow<br>
+* Dependency Injection<br>
+* RecyclerView
+
+# Testing:
+Local unit test is done for get product repo and use cases.
+
+# Notes:
+This app requires minimum Android SDK 24 and Android Studio Koala
+
+# Libraries:
+* Kotlin coroutines Executing code asynchronously.<br>
+* State Flow is a state-holder observable flow that emits the current and new state updates to its collectors.<br>
+* Hilt is a dependency injection library for Android that reduces the boilerplate of doing manual dependency injection in your project.<br>
+* Retrofit is a Type-safe HTTP client for Android, Java and Kotlin by Square.<br>
+* Gson and Moshi is a serialization/deserialization library to convert objects into JSON and back.<br>
+* Okhttp interceptor Logs HTTP requests and responses.<br>
+* Material Design Build beautiful, usable products using Material Components for Android.<br>
+* RecyclerView is a flexible and efficient view used in Android to display large sets of data. Mock web server A scriptable web server for testing HTTP clients.<br>
+* JUnit and Mockito for testing.
+
+# Screen Shots & Recording
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -13,6 +15,7 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -46,4 +49,44 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // RecyclerView
+    implementation(libs.androidx.recyclerview)
+
+    // CardView
+    implementation(libs.androidx.cardview)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+
+    // Gson library
+    implementation(libs.gson)
+    implementation(libs.converter.moshi)
+    implementation(libs.moshi)
+    implementation(libs.moshi.adapters)
+    implementation(libs.moshi.kotlin)
+
+    // Dagger Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // ViewModel injection dependencies
+    implementation(libs.androidx.hilt.lifecycle.viewmodel)
+    kapt(libs.androidx.hilt.compiler)
+
+    // Glide library
+    implementation(libs.glide)
+    kapt(libs.compiler)
+
+    // Multidex
+    implementation(libs.androidx.multidex)
 }
